59780906d106225f3c562e5c2324a505cfc67496
tracy-e <tracy.cpp@gmail.com>
2023-05-11 11:24:06 +0800

    fix for breaking changes

--- ingest.py
+++ ingest.py
@@ -9,7 +9,7 @@ from langchain.vectorstores.faiss import FAISS
 
 def ingest_docs():
     """Get documents from web pages."""
-    loader = ReadTheDocsLoader("langchain.readthedocs.io/en/latest/")
+    loader = ReadTheDocsLoader("rtdocs", features='html.parser')
     raw_documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
